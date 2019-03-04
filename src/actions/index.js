1cb20b170316ca40b6dd1aefe6b32ff13392a7e8
Stuart Gill <stuart.a.gill@gmail.com>
2019-03-03 19:52:59 -0800

    first commit, api calls to music site not working

---
+++ src/actions/index.js
@@ -0,0 +1,72 @@
+import * as types from "./../constants/ActionTypes";
+import v4 from 'uuid/v4';
+
+//name action creator the camelCase version of action's type
+export const nextLyric = (currentSongId) => ({
+  type: types.NEXT_LYRIC,
+  currentSongId
+});
+
+export const restartSong = (currentSongId) => ({
+  type: types.RESTART_SONG,
+  currentSongId
+});
+
+export const changeSong = (newSelectedSongId) => ({
+  type: types.CHANGE_SONG,
+  newSelectedSongId
+});
+
+export const requestSong = (title, localSongId) => ({
+  type: types.REQUEST_SONG,
+  title,
+  songId: localSongId
+});
+
+
+
+
+export function fetchSongId(title) {
+  return function (dispatch) {
+    const localSongId = v4();
+    dispatch(requestSong(title, localSongId));
+    title = title.replace(' ', '_');
+    return fetch('http://api.musixmatch.com/ws/1.1/track.search?&q_track=' + title + '&page_size=1&s_track_rating=desc&apikey=67ef5bdac5ca84b45a9ce5c10977c72f').then(
+      response => response.json(),
+      error => console.log('An error occurred.', error)
+    ).then(function(json) {
+      if (json.message.body.track_list.length > 0) {
+        const musicMatchId = json.message.body.track_list[0].track.track_id;
+        const artist = json.message.body.track_list[0].track.artist_name;
+        const title = json.message.body.track_list[0].track.track_name;
+        fetchLyrics(title, artist, musicMatchId, localSongId, dispatch);
+      } else {
+        console.log('We couldn\'t locate a song under that ID!');
+      }
+    });
+  };
+}
+
+export function fetchLyrics(title, artist, musicMatchId, localSongId, dispatch) {
+  return fetch('http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=' + musicMatchId + '&apikey=67ef5bdac5ca84b45a9ce5c10977c72f').then(
+    response => response.json(),
+    error => console.log('An error occurred.', error)
+  ).then(function(json) {
+    if (json.message.body.lyrics) {
+      dispatch(receiveSong(title, artist, localSongId, songArray));
+        dispatch(changeSong(localSongId));
+      } else {
+        console.log('We couldn\'t locate lyrics for this song!');
+    }
+  });
+}
+
+export const receiveSong = (title, artist, songId, songArray) => ({
+  type: types.RECEIVE_SONG,
+  songId,
+  title,
+  artist,
+  songArray,
+  receivedAt: Date.now()
+});
+
--- src/reducers/lyricChangeReducer.js
+++ src/reducers/lyricChangeReducer.js
@@ -22,8 +22,32 @@ const lyricChangeReducer = (state = initialState.songsById, action) => {
       [action.currentSongId]: newSongsByIdEntry
     });
     return newSongsByIdStateSlice;
-  default:
-    return state;
+    case types.REQUEST_SONG:
+        newSongsByIdEntry = {
+          isFetching: true,
+          title: action.title,
+          songId: action.songId
+        };
+        newSongsByIdStateSlice = Object.assign({}, state, {
+          [action.songId]: newSongsByIdEntry
+        });
+        return newSongsByIdStateSlice;
+        case types.RECEIVE_SONG:
+          newSongsByIdEntry = Object.assign({}, state[action.songId], {
+            isFetching: false,
+            receivedAt: action.receivedAt,
+            title: action.title,
+            artist: action.artist,
+            songArray: action.songArray,
+            arrayPosition: 0,
+            songId: action.songId
+          });
+          newSongsByIdStateSlice = Object.assign({}, state, {
+            [action.songId]: newSongsByIdEntry
+          });
+          return newSongsByIdStateSlice;
+        default:
+          return state;
   }
 };
 
